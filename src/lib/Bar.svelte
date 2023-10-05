2cd6fcdef19ef5716f63e7207897aa44fd084b93
Jeff Allen <jeffallen89@gmail.com>
2023-10-05 13:53:09 -0400

    add data, build charts, style

---
+++ src/lib/Bar.svelte
@@ -0,0 +1,104 @@
+<script>
+
+    import data from "../assets/data.json";
+
+    export let variable;
+
+    let filteredData = data.filter((item) => item.title === variable)[0];
+
+    let width;
+    let maxPercent = 23;
+
+    function roundToDecimal(number) {
+        return Math.round(number * 10) / 10;
+    }
+    
+    
+
+</script>
+
+
+<div class="bar" bind:clientWidth={width}>
+
+    
+
+    <svg width="100%" height="25">
+        <text x="0" y="14" font-size="18" fill="#8DBF2E">{filteredData.title}</text>
+        <line id="borderline" x1="0" y1="22" x2="{width}" y2="22"></line>
+    </svg>
+
+    {#each filteredData.data as item}
+        
+        <svg width="100%" height="45">
+            
+            <text x="0" y="13" font-size="14" fill="white">{item.Name}</text>
+
+            <rect id="eveningbar" width="{(item.Evening / maxPercent) * width}" height="20" x="0" y ="20"/>
+            <rect id="nightbar" width="{(item.Night / maxPercent) * width}" height="20" x="{(item.Evening / maxPercent) * width}" y ="20"/>
+            <rect id="bardivider" width="2" height="20" x="{(item.Evening / maxPercent) * width - 1}" y ="20"/>
+
+            <text x="5" y="35" font-size="14" fill="black">{roundToDecimal(item.Evening)}%</text>
+            <text x="{(item.Evening / maxPercent) * width + 5}" y="35" font-size="14" fill="black">{roundToDecimal(item.Night)}%</text>
+
+        </svg>
+
+    {/each}
+
+    <svg width="100%" height="65">
+        <line id="xaxis" x1="0" y1="1" x2="{width}"  y2="1"></line>
+
+        <line id="xaxis" x1="1" y1="1" x2="1"  y2="8"></line>
+        <line id="xaxis" x1="{width - 1}" y1="1" x2="{width - 1}"  y2="8"></line>
+        <line id="xaxis" x1="{width * 0.25}" y1="1" x2="{width * 0.25}"  y2="8"></line>
+        <line id="xaxis" x1="{width * 0.5}" y1="1" x2="{width * 0.5}"  y2="8"></line>
+        <line id="xaxis" x1="{width * 0.75}" y1="1" x2="{width * 0.75}"  y2="8"></line>
+
+        <text x="0" y="21" font-size="14" fill="#D0D1C9" text-anchor="left">0%</text>
+        <text x="{width * 0.25}" y="21" font-size="14" fill="#D0D1C9" text-anchor="middle">5%</text>
+        <text x="{width * 0.5}" y="21" font-size="14" fill="#D0D1C9" text-anchor="middle">10%</text>
+        <text x="{width * 0.75}" y="21" font-size="14" fill="#D0D1C9" text-anchor="middle">15%</text>
+        <text x="{width}" y="21" font-size="14" fill="#D0D1C9" text-anchor="end">20%</text>
+
+        <text x="{width / 2}" y="40" font-size="14" fill="#D0D1C9" text-anchor="middle">Percent of all journey-to-work trips</text>
+
+        <rect id="eveningbar" width="10" height="20" x="0" y ="45"/>
+        <text x="15" y="60" font-size="14" fill="#D0D1C9" text-anchor="left">Evening Trips</text>
+
+        <rect id="nightbar" width="10" height="20" x="110" y ="45"/>
+        <text x="125" y="60" font-size="14" fill="#D0D1C9" text-anchor="left">Night Trips</text>
+
+    </svg>
+
+</div>
+
+
+<style>
+
+    .bar {
+        max-width: 750px;
+        width: 100%;
+        min-height: 20px;
+        background-color: none;
+        margin: 0px;
+        padding: 0px;
+        margin-bottom: 5px;
+    }
+    #eveningbar {
+        fill: var(--brandLightBlue);
+    }
+    #nightbar {
+        fill: var(--brandYellow);
+    }
+    #bardivider {
+        fill: var(--brandWhite);
+    }
+    #borderline {
+        stroke: var(--brandLightGreen);
+        stroke-width:1
+    }
+    #xaxis {
+        stroke: var(--brandGray);
+        stroke-width:1
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 
     import TopSofC from "../lib/TopSofC.svelte";
+    import Bar from "../lib/Bar.svelte";
     import "../assets/global-styles.css";
-
+    
 </script>
 
 <TopSofC/>
@@ -17,9 +18,27 @@
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        <Bar variable="Overall"/>
         <p>
             Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum i[r]ure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
         </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Bar variable="Low-Income Status"/>
+        <p>
+            Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum i[r]ure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+        </p>
+        <Bar variable="Visibile Minority"/>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Bar variable="Immigrants"/>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Bar variable="Employment Sector"/>
     </div>
+
 </main>
 
