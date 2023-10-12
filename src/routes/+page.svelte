744fa7bec959c5486fa7fc7654d4d84707bbbb20
Jeff Allen <jeffallen89@gmail.com>
2023-10-12 17:11:34 -0400

    update text, add city graphic, update clocks:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import Bar from "../lib/Bar.svelte";
     import "../assets/global-styles.css";
     import moon from '../assets/moon.svg';
+    import sofcLongCity from '../assets/sofc-long-city.svg';
     import clockAM from '../assets/clock-am.svg';
     import clockPM from '../assets/clock-pm.svg';
     
@@ -13,27 +14,32 @@
 
 <main>
     <div class="title">
-        <div class="left-title">
-            <h1>Transportation Justice for Night and Evening Shift Workers in Canada</h1>
-            <p>Matt Palm & Jeff Allen</p>
-            <p>October 2023</p>
+        <div class="title-columns">
+            <div class="left-title">
+                <h1>Transportation Justice for Night and Evening Shift Workers in Canada</h1>
+                <p>Matt Palm & Jeff Allen</p>
+                <p>October 2023</p>
+            </div>
+            <div class="right-title">
+                <img width=100 height=100 src={moon} alt="moon">
+            </div>
         </div>
-        <div class="right-title">
-            <img width=100 height=100 src={moon} alt="moon">
-        </div>
+        <img width=100% height=71 src={sofcLongCity} alt="sofcLongCity">
     </div>
+    
     <div class="text">
+        
         <p>
-            Night and evening shift workers play pivotal roles in our economy, keeping the lights on in sectors like healthcare, emergency services, public safety, logistics, and infrastructure. Yet, a gap exists in the research dedicated to understanding their transportation needs from a justice standpoint. Our latest <a href="https://doi.org/10.1016/j.trd.2023.103875">study</a> seeks to bridge this discrepancy. 
+            Night and evening shift workers play pivotal roles in our economy, keeping the lights on in sectors like healthcare, emergency services, public safety, logistics, and infrastructure. Yet, a gap exists in the research dedicated to understanding their transportation needs from a justice standpoint. Our recent <a href="https://doi.org/10.1016/j.trd.2023.103875">study</a> seeks to bridge this discrepancy. 
         </p>
 
         <h2>Why is Transportation Justice Important for Shift Workers?</h2>
         
         <p>
-            <span class="bold">1) Safety:</span> Shift workers face higher chances of vehicle collisions. Offering them alternative commuting options can potentially reduce the number of road injuries or fatalities, reinforcing public health efforts. 
+            <span class="bold">1) Safety:</span> Shift workers face higher chances of <a href="https://doi.org/10.1016/S0001-4575(02)00007-6">vehicle collisions</a>. Offering them alternative commuting options can potentially reduce the number of road injuries or fatalities, reinforcing public health efforts. 
         </p>
         <p>  
-            <span class="bold">2) Health & Social Integration:</span> Shift work is linked with adverse physical and mental health consequences and heightened feelings of social isolation. Enhancing these workers’ access to social and economic opportunities can help address these issues.
+            <span class="bold">2) Health & Social Integration:</span> Shift work is linked with adverse <a href="https://doi.org/10.1136/bmj.i5210">physical</a> and <a href="https://link.springer.com/article/10.1007/s00420-019-01434-3">mental health</a> consequences and heightened feelings of <a href="https://link.springer.com/article/10.1007/s40675-018-0114-7">social isolation</a>. Enhancing these workers’ access to social and economic opportunities can help address these issues.
         </p> 
         <p>
             <span class="bold">3) Equity and Representation:</span> Global research hints at a trend where shift workers disproportionately belong to equity-deserving communities. Our study is among the first to systematically document this in Canada.
@@ -42,7 +48,8 @@
         <h2>Understanding Shifts?</h2>
 
         <p>
-            Deciphering who exactly works shifts can be tricky, mainly because primary data sources, like the long-form Census, lack standard questions about workers' schedules. We overcame this limitation by cross-referencing with the Canadian General Social Survey's Time Use wave. This approach helped us categorize workers into three shifts based on their work arrival time: 
+            
+            The Canadian General Social Survey's <a href="https://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&SDDS=4503">Time Use</a> data asks whether people work day, evening, or night shifts as well as their work arrival time. We use these to create a classification of shift type based on work arrival times.
         </p> 
        
         <img width=150 height=150 src={clockAM} alt="clockAM" style="margin-right: 30px;">
@@ -70,7 +77,8 @@
         <h2>Demographics of Shift Workers</h2>
 
         <p>
-            Our findings show that fewer than 10% of Canadians were engaged in night or evening shifts at the census time. 
+            We then sought out to analyze how working evening and night shifts varies among different demographic groups. 
+            The General Social Survey is a relatively small sample, so we decided to use the Canadian long-form census, a 25% of all Canadians. We are able to identify night and evening shift workers in the 2016 Canadian census, based on self-reported arrival time, and the classification above. Our findings show that fewer than 10% of Canadians were engaged in night or evening shifts at the census time, however this varies for different demographic groups.
         </p>
         <Bar variable="Overall"/>
         <p>
@@ -89,6 +97,13 @@
             <span class="bold">Sectors of Employment:</span> Evening workers are overrepresented in sales, services, arts, culture, manufacturing, and utilities, while night shift workers are more concentrated in vital areas like health and manufacturing or utilities. 
         </p>
         <Bar variable="Employment Sector"/>
+
+        <h2>Conclusions</h2>
+
+        <p>
+            Night and evening shift workers, often from equity-deserving communities, are the backbone of many essential services in Canada. Addressing their unique needs and challenges can pave the way for a more sustainable, equitable, and safe future for all. 
+        </p> 
+
     </div>
 
 </main>
